41a28f8bccc24795f8b2a71f469a657ac4bee1eb
Olexandr Lytvyn <klesh308@gmail.com>
2023-11-08 18:42:46 +0200

    Changed

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -22,7 +22,7 @@ public class Board extends JPanel implements ActionListener {
     private Pacman pacman;
 
     public static final int BLOCK_SIZE = 24;
-    public static final int N_BLOCKS = 15;
+    public static final int N_BLOCKS = 17;
     public static final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
 
     public static int score;
--- src/main/java/org/example/characters/Ghost.java
+++ src/main/java/org/example/characters/Ghost.java
@@ -11,60 +11,19 @@ import static org.example.Board.inGame;
 public class Ghost {
     private static int N_GHOSTS = 4;
     private Image ghost = new ImageIcon("src/main/resources/images/ghost.png").getImage();
-    private int x, y, dx, dy, speed;
+    protected int x, y, dx, dy, speed;
+    protected final int ATTACK = 280;
+    protected final int SCATTER = 100;
+    protected int countdownTimer = 0;
+    protected boolean isAttacking = false;
+    private int lastDirectionY = 0;
+    private int lastDirectionX = 0;
     public void moveGhost(Graphics2D g2d, Pacman pacman, Board board) {
-        short i;
-        int posx;
-        int posy;
-        int count;
 
-        int[] dx_temp = new int[4], dy_temp = new int[4];
 
         if (x % BLOCK_SIZE == 0 && y % BLOCK_SIZE == 0) {
-            posx = x / BLOCK_SIZE;
-            posy = y / BLOCK_SIZE;
-            count = 0;
-
-            if ((posx != 0 && Board.screenData[posx - 1][posy] != 2) && dx != 1) {
-                dx_temp[count] = -1;
-                dy_temp[count] = 0;
-                count++;
-            }
-
-            if ((posy != 0 && Board.screenData[posx][posy - 1] != 2) && dy != 1) {
-                dx_temp[count] = 0;
-                dy_temp[count] = -1;
-                count++;
-            }
-
-            if ((posx != Board.screenData.length - 1 && Board.screenData[posx + 1][posy] != 2) && dx != -1) {
-                dx_temp[count] = 1;
-                dy_temp[count] = 0;
-                count++;
-            }
-
-            if ((posy != Board.screenData.length - 1 && Board.screenData[posx][posy + 1] != 2) && dy != -1) {
-                dx_temp[count] = 0;
-                dy_temp[count] = 1;
-                count++;
-            }
-
-            if (count == 0) {
-                dx = -dx;
-                dy = -dy;
-            } else {
-
-                count = (int) (Math.random() * count);
-
-                if (count > 3) {
-                    count = 3;
-                }
-
-                dx = dx_temp[count];
-                dy = dy_temp[count];
-            }
+            getAIMove(pacman);
         }
-
 
         x = x + (dx * speed);
         y = y + (dy * speed);
@@ -75,6 +34,111 @@ public class Ghost {
                 && inGame) {
 
             pacman.setDying(true);
+        }
+    }
+
+    public boolean moveIsAllowed() {
+//        System.out.println(Board.screenData[(x / BLOCK_SIZE + dx)][(y / BLOCK_SIZE + dy)]);
+        return ((x/ BLOCK_SIZE + dx >= 0 && (x / BLOCK_SIZE + dx)  < Board.screenData.length) &&
+                (y / BLOCK_SIZE+ dy >= 0 && (y / BLOCK_SIZE + dy)  < Board.screenData.length)
+                && Board.screenData[(x / BLOCK_SIZE + dx)][(y / BLOCK_SIZE + dy)] != 2);
+    }
+
+    protected void flipAttack(){
+        if(isAttacking){
+            countdownTimer = SCATTER;
+        } else{
+            countdownTimer = ATTACK;
+        }
+        isAttacking = !isAttacking;
+    }
+
+    protected void getAIMove(Pacman pm)
+    {
+        // as of now, this ghost just tries to get to you as fast as possible
+        // with some work, it could end up being very smart
+        // so for now this is just an example for one way of doing this
+
+        int curX = this.x;
+        int curY = this.y;
+        int targetX, targetY;
+        if(!isAttacking){
+//            System.out.println("!isAttacking");
+            targetX = Board.screenData.length*BLOCK_SIZE - pm.getX();
+            targetY = Board.screenData.length*BLOCK_SIZE - pm.getY();
+            countdownTimer --;
+        } else {
+//            System.out.println("Attacking!!!");
+            targetX = pm.getX();
+            targetY = pm.getY();
+            countdownTimer --;
+        }
+
+            if(countdownTimer <= 0){
+                flipAttack();
+            }
+
+            tryMove(curX, curY, targetX, targetY);
+        lastDirectionX = dx;
+        lastDirectionY = dy;
+    }
+
+
+    protected void tryMove(int curX, int curY, int targetX, int targetY){
+        int horizontalDifference = curX - targetX;
+        int verticalDifference = curY - targetY;
+        int preferredHorizontal = horizontalDifference > 0 ? -1 : 1;
+        int preferredVertical = verticalDifference > 0 ? -1 : 1;
+//        System.out.println(Math.abs(verticalDifference) - Math.abs(horizontalDifference));
+        boolean verticalMoreImportant = Math.abs(verticalDifference) > Math.abs(horizontalDifference);
+        if (verticalMoreImportant) {
+            dy = preferredVertical;
+            dx = 0;
+        }
+        else {
+            dx = preferredHorizontal;
+            dy = 0;
+        }
+        if (!this.moveIsAllowed()) {
+            if (verticalMoreImportant) {
+//                System.out.println("Vertical");
+                if (lastDirectionX == -1 || lastDirectionX == 1) {
+                    dx = lastDirectionX;
+                    dy = 0;
+                    if (!this.moveIsAllowed())
+                        dx = dx == -1 ? 1 : -1;
+                } else {
+//                    System.out.println("Else 1");
+                    dx = preferredHorizontal;
+                    dy = 0;
+                    if (!this.moveIsAllowed()) {
+                        dx = preferredHorizontal == -1 ? 1 : -1;
+                        if (!this.moveIsAllowed()) {
+                            dy = preferredVertical == -1 ? 1 : -1;
+                            dx = 0;
+                        }
+                    }
+                }
+            } else {
+//                System.out.println("Horizontal");
+                if ((lastDirectionY == -1) || (lastDirectionY == 1)) {
+                    dy = lastDirectionY;
+                    dx = 0;
+                    if (!this.moveIsAllowed())
+                        dy = dy == -1 ? 1 : -1;
+                } else {
+//                    System.out.println("Else 2");
+                    dy = preferredVertical;
+                    dx = 0;
+                    if (!this.moveIsAllowed()) {
+                        dy = preferredVertical == -1 ? 1 : -1;
+                        if (!this.moveIsAllowed()) {
+                            dx = preferredHorizontal == -1 ? 1 : -1;
+                            dy = 0;
+                        }
+                    }
+                }
+            }
         }
     }
 
